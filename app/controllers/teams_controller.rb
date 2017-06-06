2f6715c2a45f94d07af3d701744bfcdee53cf327
dkhf92 <didrik@forsberg.to>
2017-06-06 17:23:23 +0100

    allowing teams to be updated

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -46,6 +46,6 @@ class TeamsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def team_params
-      params.fetch(:team, {})
+      params.require(:team).permit(:name, :description, :image)
     end
 end
