3102c0804d19d31948a538e6213d992a0581f59b
dkhf92 <didrik@forsberg.to>
2017-06-06 18:19:39 +0100

    updated images in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ u5 = User.create!(username: "Louie", first_name: "Lo", last_name: "i", email: "w
 
 
 t1 = Team.create!(name: "TeamOne", image: "https://pbs.twimg.com/profile_images/378800000797610573/af1e7e90714a54249d00c6eae8b85f22_400x400.jpeg", description: "we the best")
-t1 = Team.create!(name: "TeamTwo", image: "https://i.ytimg.com/vi/prALrHUJ8Ns/hqdefault.jpg", description: "Hi. Looking for last player for our 5 a side team. You have to play defense.")
-t1 = Team.create!(name: "TeamThree", image: "https://i.ytimg.com/vi/prALrHUJ8Ns/hqdefault.jpg", description: "Looking for players to play 5 a side. 19:00-21:00 on sundays")
-t1 = Team.create!(name: "TeamFour", image: "http://bcdn.sadanduseless.com/wp-content/uploads/2014/06/funny-cat1.jpg", description: "We are serious")
+t1 = Team.create!(name: "TeamTwo", image: "https://pbs.twimg.com/profile_images/848471660860538880/pevXVsIp_400x400.jpg", description: "Hi. Looking for last player for our 5 a side team. You have to play defense.")
+t1 = Team.create!(name: "TeamThree", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjlexmzVjqlTanY_4eNpIsZJnXgJIpk5YDHG1fUOb6qJgahFMI", description: "Looking for players to play 5 a side. 19:00-21:00 on sundays")
+t1 = Team.create!(name: "TeamFour", image: "https://pbs.twimg.com/profile_images/589104941047934976/Tzmj2HsY.jpg", description: "We are serious")
 t1 = Team.create!(name: "TeamFive", image: "http://images.nike.com/is/image/DotCom/PHN_PS/SC2987_100_A/strike-premier-league-football.png?fmt=png-alpha", description: "Team number five.")
