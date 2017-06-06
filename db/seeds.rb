487ff955e6866bd1c80357a11f01eedf1ef6c9e6
dkhf92 <didrik@forsberg.to>
2017-06-06 14:38:13 +0100

    added some data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@ u5 = User.create!(username: "Louie", first_name: "Lo", last_name: "i", email: "w
 
 
 t1 = Team.create!(name: "TeamOne", image: "https://vignette2.wikia.nocookie.net/googology/images/1/16/1-0.jpg/revision/latest/scale-to-width-down/220?cb=20151019095337", description: "we the best")
+t1 = Team.create!(name: "TeamTwo", image: "https://i.ytimg.com/vi/prALrHUJ8Ns/hqdefault.jpg", description: "second")
+t1 = Team.create!(name: "TeamThree", image: "https://i.ytimg.com/vi/prALrHUJ8Ns/hqdefault.jpg", description: "three")
+t1 = Team.create!(name: "TeamFour", image: "http://bcdn.sadanduseless.com/wp-content/uploads/2014/06/funny-cat1.jpg", description: "four")
+t1 = Team.create!(name: "TeamFive", image: "https://ichef.bbci.co.uk/images/ic/480x270/p05394v7.jpg", description: "five")
