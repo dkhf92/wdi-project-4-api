3933392259cd624c26f5e4091f4990fa2531fc92
dkhf92 <didrik@forsberg.to>
2017-06-09 12:09:56 +0100

    updated seedsfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-u1 = User.create!(username: "Lorenzo", first_name: "Lo", last_name:"Renzo", email: "a@a.com", password: "password", password_confirmation: "password")
-u2 = User.create!(username: "Mo", first_name: "M", last_name:"O", email: "b@b.com", password: "password", password_confirmation: "password")
-u3 = User.create!(username: "Sophie", first_name: "Sop", last_name:"hi", email: "c@c.com", password: "password", password_confirmation: "password")
-u4 = User.create!(username: "Tim", first_name: "Ti", last_name:"M", email: "d@d.com", password: "password", password_confirmation: "password")
-u5 = User.create!(username: "Louie", first_name: "Lo", last_name: "i", email: "w@w.com", password: "password", password_confirmation: "password")
+u1 = User.create!(username: "Zlatan", first_name: "Zlatan", last_name:"Ibrahimovich", email: "a@a.com", password: "password", password_confirmation: "password")
+u2 = User.create!(username: "Messi", first_name: "Lionel", last_name:"Messi", email: "b@b.com", password: "password", password_confirmation: "password")
+u3 = User.create!(username: "Cristiano", first_name: "Cristiano", last_name:"Ronaldo", email: "c@c.com", password: "password", password_confirmation: "password")
+u4 = User.create!(username: "Sadio", first_name: "Sadio", last_name:"Mane", email: "d@d.com", password: "password", password_confirmation: "password")
+u5 = User.create!(username: "Petr", first_name: "Petr", last_name: "Cech", email: "w@w.com", password: "password", password_confirmation: "password")
 
 t1 = Team.create!(name: "TeamOne", image: "https://pbs.twimg.com/profile_images/378800000797610573/af1e7e90714a54249d00c6eae8b85f22_400x400.jpeg", description: "we the best", creator_id: 1)
 t1 = Team.create!(name: "TeamTwo", image: "https://pbs.twimg.com/profile_images/848471660860538880/pevXVsIp_400x400.jpg", description: "Hi. Looking for last player for our 5 a side team. You have to play defense.", creator_id: 1)
